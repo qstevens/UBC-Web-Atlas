6c3c76128036aa3d3eeefa3f1234982a263b4651
Stevens Qiu <stevensqiu@gmail.com>
2019-02-24 16:57:44 -0800

    fixed bug where sections not added to authoritative courses

--- CourseInfo/Course.js
+++ CourseInfo/Course.js
@@ -6,7 +6,7 @@ class Course {
         this.course_number = courseTree.course_number;
         this.course_title = courseTree.course_title;
         this.course_link = courseTree.course_link;
-        if (courseTree.courses != undefined && courseTree.courses != null) 
+        if (courseTree.sections != undefined && courseTree.sections != null) 
             this.sections = Array.from(Object.keys(courseTree.sections));
     }
 
