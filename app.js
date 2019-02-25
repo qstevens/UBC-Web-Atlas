dc46fc67f0f521d31cce68142609a0e35035e64b
Stevens Qiu <stevensqiu@gmail.com>
2019-02-24 17:00:13 -0800

    removed unused function

--- app.js
+++ app.js
@@ -49,8 +49,6 @@ MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
       }
     }
   }
-
-  // console.log("Ok here");
   
   // Insert collections into database
   subjectTreeCollection.insertMany(subjectTrees)
@@ -64,56 +62,6 @@ MongoClient.connect(uri, { useNewUrlParser: true }, function(err, client) {
   .then(sectionCollection.insertMany(sections))
 
   .then(function () {
-    // console.log("Ok here too");
     client.close();
   });
 });
-
-const insertDocuments = function(db, callback) {
-
-  // Create collections in database
-  const subjectTreeCollection = db.collection('treeSubjects');
-  const subjectCollection = db.collection('subjects');
-
-  const courseTreeCollection = db.collection('treeCourses');
-  const courseCollection = db.collection('courses');
-
-  const sectionCollection = db.collection('sections');
-
-  // Generate all documents for each collection
-  let jsonTree = JSON.parse(fs.readFileSync('./data/UBC-Courses-' + dbName + '.json'));
-  
-  let subjectTrees = Object.keys(jsonTree).map(key => jsonTree[key]);
-  let subjects = [];
-
-  let courseTrees = [];
-  let courses = [];
-
-  let sections = [];
-
-
-  for (let subject of subjectTrees) {
-    // console.log(Object.keys(subject.courses));
-    subjects.push(new Subject(subject));
-    for (let course of Object.values(subject.courses)) {
-      courseTrees.push(course);
-      courses.push(new Course(course));
-      for (let section of Object.values(course.sections)) {
-        sections.push(section);
-      }
-    }
-  }
-  
-  // Insert collections into database
-  subjectTreeCollection.insertMany(subjectTrees)
-
-  .then(subjectCollection.insertMany(subjects))
-
-  .then(courseTreeCollection.insertMany(courseTrees))
-
-  .then(courseCollection.insertMany(courses))
-
-  .then(sectionCollection.insertMany(sections));
-
-  // callback();
-}
